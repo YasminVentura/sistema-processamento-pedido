1afa8e7c58460780f3e837b77537c84a9856868b
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-05 13:24:54 -0300

    Projeto criado

---
+++ src/main/java/com/cursoJavaU/aula1SpringBoot/Aula1SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cursoJavaU.aula1SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula1SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula1SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursoJavaU/aula1SpringBoot/Aula1SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoJavaU.aula1SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula1SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
